b8d578b96e1667741eb1628aca06654f7ed48382
Pierre Collinet <pi.collinet@gmail.com>
2017-12-01 17:47:34 +0100

    base settings

--- barreauproject/aws/conf.py
+++ barreauproject/aws/conf.py
@@ -31,7 +31,7 @@ AWS_HEADERS = {
 from botocore.client import Config
 
 
-s3 = boto3.resource(
+s3 = boto.resource(
     's3',
     aws_access_key_id=AWS_ACCESS_KEY_ID,
     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
