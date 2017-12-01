f1f1a4891106347c1cd547991392cd2f4940cc7b
Pierre Collinet <pi.collinet@gmail.com>
2017-12-01 16:57:18 +0100

    ...

--- barreauproject/settings/production.py
+++ barreauproject/settings/production.py
@@ -19,7 +19,7 @@ BASE_DIR     = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = "app-root/repo/wsgi/static"
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
