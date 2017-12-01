1d39bec33e97419e4ff60f613cdeae6b5a4ab174
Pierre Collinet <pi.collinet@gmail.com>
2017-12-01 14:49:15 +0100

    ...

--- barreauproject/settings/production.py
+++ barreauproject/settings/production.py
@@ -21,6 +21,8 @@ BASE_DIR     = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
+from barreauproject.aws.conf import *
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -39,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 )
 
 MIDDLEWARE_CLASSES = (
