5289c9f846986c0769eb73bb9e7efa72000fc0c6
Pierre Collinet <pi.collinet@gmail.com>
2017-12-01 17:38:02 +0100

    base settings

--- barreauproject/aws/conf.py
+++ barreauproject/aws/conf.py
@@ -8,6 +8,9 @@ AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
 
+use-sigv4 = True
+
+
 DEFAULT_FILE_STORAGE = 'barreauproject.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'barreauproject.aws.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'barreau-project'
