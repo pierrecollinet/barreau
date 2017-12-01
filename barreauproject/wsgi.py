4945638a4e5860552dc6da3505623a24b679e1c9
Pierre Collinet <pi.collinet@gmail.com>
2017-12-01 17:56:12 +0100

    base settings

--- barreauproject/wsgi.py
+++ barreauproject/wsgi.py
@@ -1,6 +1,5 @@
-
 """
-WSGI config for blocusassistance project.
+WSGI config for barreauproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,33 +9,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-import sys
-
-
-from django.core.wsgi import get_wsgi_application
-
-reload(sys)
-sys.setdefaultencoding("utf-8")
-
-
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "barreauproject.settings")
 
-
 application = get_wsgi_application()
-
-
-try:
-   # from dj_static import Cling
-    from whitenoise.django import DjangoWhiteNoise
-    from dj_static import Cling
-
-   # application = Cling(get_wsgi_application())
-    application = get_wsgi_application()
-    application = DjangoWhiteNoise(application)
-
-except:
-    pass
-
