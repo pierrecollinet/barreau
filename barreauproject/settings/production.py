811c7f54bbffca81b352f846869fcd6bfea7789b
Pierre Collinet <pi.collinet@gmail.com>
2017-12-01 16:08:23 +0100

    ...

--- barreauproject/settings/production.py
+++ barreauproject/settings/production.py
@@ -131,3 +131,13 @@ SECURE_FRAME_DENY               = True
 
 
 from barreauproject.aws.conf import *
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = "app-root/repo/wsgi/static"
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
