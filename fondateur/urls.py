898dc4019bb2db75d540a50335a5b67253f7c56f
Pierre Collinet <pi.collinet@gmail.com>
2018-01-23 18:49:29 +0100

    remove duplicate files

--- fondateur/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from .views import dashboard
-
-urlpatterns = [
-    url('^$', dashboard, name="tableau-de-bord"),
-    url('^tableau-de-bord$', dashboard, name="tableau-de-bord"),
-]
