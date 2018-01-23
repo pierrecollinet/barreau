ed46a8e9e3f9d7da3c97527b99cbe7a141c48ab2
pierreboonen <pierre.boonen@passelapremiere.be>
2018-01-23 10:33:18 +0100

    first commit

--- src/barreauproject/settings/production.py
+++ src/barreauproject/settings/production.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-print('JE SUIS EN PRODUCTION')
+print('JE SUIS EN PRODUCTION !')
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
